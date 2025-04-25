3b2d09cdadeea99128654b12766bea3483c93f3a
Sanjith A S <sanjith1112006@gmail.com>
2025-04-25 21:56:13 +0530

    Initial commit - Pandas-Sales-Analysis

---
+++ README.md
@@ -0,0 +1,68 @@
+# 🛒 Superstore Sales Data Analysis only using pandas
+
+This project involves Exploratory Data Analysis (EDA) of a retail Superstore dataset using only **Pandas**. The goal is to extract meaningful insights into sales performance, profitability, customer segments, and product trends — all without using any visualization tools (yet).
+
+---
+
+## 📁 Dataset
+
+The dataset used is the **Sample Superstore** dataset, which includes:
+
+- **Orders**: Product name, Category, sub-Category, Sales, Quantity, Discount, Profit
+- **Customer info**: Segment, Region, State
+- **Shipping info**: Ship Mode, Ship Date, 
+
+---
+
+## 🧰 Tools Used
+
+- Python 🐍
+- Pandas 🐼
+- Jupyter Notebook 💻
+
+---
+
+## 📊 Key Analysis Steps
+
+1. **Initial Overview of the Dataset**
+2. **Sales and Profit by Region**
+3. **Sub-Category Performance**
+4. **Segment-wise Analysis**
+5. **State-wise Profitability**
+6. **Discount vs Profit Analysis**
+7. **Top Loss-Making Products**
+8. **Shipping Mode Impact on Profit**
+9. **Category-Level Profitability**
+10. **Summary of Key Metrics**
+
+---
+
+## 📌 Insights & Highlights
+
+- **Technology** is the most profitable category, with high margins.
+- **Furniture** generates high sales but low profit often due to heavy discounts.
+- **Standard Class** shipping mode gives the lowest average profit per order.
+- Several products have **consistently negative profits**, especially in the **Bookcases and Tables** sub-categories.
+- Some **states like Texas** showed a net **loss**, despite high sales.
+- Discount ranges above **30%** are often linked with unprofitable sales.
+
+---
+
+## 📐 Summary Metrics
+
+| Metric                           | Value / Description                    |
+|----------------------------------|----------------------------------------|
+| Total Sales                      | $ 2297200.86                           |
+| Total Profit                     | $ 286397.02                            |
+| Overall Profit Margin (%)        | 12.47%                                 |
+| Most Profitable Region           | West                                   |
+| Most Profitable Sub-Category     | Copiers                                |
+| Top 3 Profitable States          | California, New York, Washington       |
+| Worst Performing Product         | "Cubify Cubex 3D Printer Double Head"  |
+| Ship Mode with Lowest Profit     | Same Day                               |
+| Most Profitable Category         | Technology                             |
+| Discount Range Causing Losses    | Above 30%                              |
+
+
+
+
---
+++ sales_analysis.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+x=pd.read_csv('dataset/Superstore.csv',encoding='latin1')
+x=x.drop_duplicates()
+pd.set_option('display.max_columns', None)
+x['Order Date'] = pd.to_datetime(x['Order Date'])
+x['Year'] = x['Order Date'].dt.year
+x['Month'] = x['Order Date'].dt.month
+x['Day'] = x['Order Date'].dt.day
+x['Weekday'] = x['Order Date'].dt.day_name()
+
+''' 
+#category wise sales and profit analysis
+category_sales_profit = x.groupby('Category')[['Sales','Profit']].sum().sort_values(by='Profit', ascending=False)
+category_sales_profit['avg_discount (%)'] = x.groupby('Category')['Discount'].mean()*100
+category_sales_profit['profit_margin (%)'] = (category_sales_profit['Profit'] / category_sales_profit['Sales']) * 100
+print(category_sales_profit)
+
+#Shipping Analysis
+shipping_analysis = x.groupby('Ship Mode')[['Sales','Profit']].sum().sort_values(by='Profit', ascending=False)
+avg_profit_shipping = x.groupby('Ship Mode')[['Profit']].mean().sort_values(by='Profit', ascending=False)
+print(shipping_analysis)
+print(avg_profit_shipping)
+
+#Loss making products 
+product_loss= x.groupby('Product Name')[['Sales','Profit']].sum().sort_values(by='Profit', ascending=True)
+product_loss= product_loss[product_loss['Profit']<0]
+product_loss['profit_margin (%)'] = (product_loss['Profit'] / product_loss['Sales']) * 100
+#product_loss.sort_values(by='profit_margin (%)', ascending=True, inplace=True)
+print(product_loss.head(10))
+
+#discount analysis
+discount_profit= x.groupby('Discount')[['Profit']].mean().sort_values(by='Discount', ascending=True)
+discount_sales_profit= x.groupby('Discount')[['Profit','Sales']].mean().sort_values(by='Discount', ascending=True)
+print(discount_profit)
+print(discount_sales_profit)
+
+#State wise analysis
+state_sales_profit = x.groupby('State')[['Sales','Profit']].sum().sort_values(by='Profit', ascending=False)
+state_sales_profit['profit_margin (%)'] = (state_sales_profit['Profit'] / state_sales_profit['Sales']) * 100
+state_sales_profit.sort_values(by='profit_margin (%)', ascending=False, inplace=True)
+print(state_sales_profit)
+
+#Segment wise analysis
+segment_sales_profit = x.groupby('Segment')[['Sales','Profit']].sum().sort_values(by='Profit', ascending=False)
+segment_sales_profit['profit_margin (%)'] = (segment_sales_profit['Profit'] / segment_sales_profit['Sales']) * 100
+segment_sales_profit.sort_values(by='profit_margin (%)', ascending=False, inplace=True)
+print(segment_sales_profit)
+
+#Sub Category Sales Analysis
+sub_category_sales = x.groupby('Sub-Category')[['Sales','Profit']].sum().sort_values(by='Profit', ascending=False)
+print(sub_category_sales)
+
+#overall profit margin and sales analysis
+total_sales = x['Sales'].sum()
+total_profit = x['Profit'].sum()
+profit_margin = (total_profit / total_sales) * 100
+print(f'Total Sales: ${total_sales:.2f}')
+print(f'Total Profit: ${total_profit:.2f}')
+print(f'Profit Margin: {profit_margin:.2f}%')
+'''
